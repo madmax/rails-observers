af08c1dd5fb76310f606a90d2b928b70ccfa3c58
Grzegorz Derebecki <grzegorz.derebecki@fdb.pl>
2016-06-20 09:39:27 +0200

    ActionDispatch::Reloader.to_prepare -> ActiveSupport::Reloader.to_prepare

--- lib/rails/observers/railtie.rb
+++ lib/rails/observers/railtie.rb
@@ -20,7 +20,7 @@ module Rails
       config.after_initialize do |app|
         ActiveSupport.on_load(:active_record) do
           ActiveRecord::Base.instantiate_observers
-          ActionDispatch::Reloader.to_prepare do
+          ActiveSupport::Reloader.to_prepare do
             ActiveRecord::Base.instantiate_observers
           end
         end
